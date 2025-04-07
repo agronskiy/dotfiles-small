b8ba8187e1cdeb8101725479bd3994dd1ac0d72e
Alex Gronskiy <agronskiy@nvidia.com>
2025-04-07 12:33:33 +0200

    fix: fixes installers order

--- linux/all.linux.yasli.sh
+++ linux/all.linux.yasli.sh
@@ -1,5 +1,62 @@
 #!/usr/bin/env bash
 
+install_tmux() {
+    [ -d "$HOME/.local/bin/tmux-install" ] && rm -rf "$HOME/.local/bin/tmux-install"
+    mkdir -p "$HOME/.local/bin/tmux-install"
+    (
+        cd $HOME/.local/bin/tmux-install \
+        && cp $DOTFILES/tmux/tmux.linux-amd64.tar.gz . \
+        && tar -xvf tmux.linux-amd64.tar.gz \
+        && ln -s --force "$(realpath ./tmux.linux-amd64)" "$HOME/.local/bin/tmux"
+    )
+}
+exists_tmux() {
+  [ -x "$(command -v tmux)" ] && tmux -V | grep -q "3.5"
+}
+install_wrapper "tmux" install_tmux exists_tmux
+
+# tmux-tpm
+install_tmux_tpm() {
+    mkdir -p $HOME/.tmux/plugins
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm \
+        && TMUX_PLUGIN_MANAGER_PATH=$HOME/.tmux/plugins /$HOME/.tmux/plugins/tpm/bin/install_plugins
+}
+exists_tmux_tpm () {
+    [ -d $HOME/.tmux/plugins/tpm/.git ]
+}
+update_tmux_tpm () {
+    ( cd $HOME/.tmux/plugins/tpm && git pull &> /dev/null )
+}
+install_wrapper "tmux tpm" \
+    install_tmux_tpm \
+    exists_tmux_tpm \
+    update_tmux_tpm
+
+# rg
+install_ripgrep() {
+    [ -d "$HOME/.local/bin/ripgrep-install" ] && rm -rf "$HOME/.local/bin/ripgrep-install"
+    mkdir -p "$HOME/.local/bin/ripgrep-install"
+    cd $HOME/.local/bin/ripgrep-install \
+    && curl -LO 'https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz' \
+    && tar xzvf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz \
+    && ln -s --force "$(realpath ./ripgrep-13.0.0-x86_64-unknown-linux-musl/rg)" "$HOME/.local/bin/rg"
+}
+exists_ripgrep() {
+    [ -x "$(command -v rg)" ]
+}
+install_wrapper "ripgrep" install_ripgrep exists_ripgrep
+
+# fzf
+install_fzf() {
+    rm -rf "$HOME/.fzf" || true
+    git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf" \
+        && "$HOME/.fzf/install" --key-bindings --completion --no-update-rc
+}
+exists_fzf() {
+    [ -x "$HOME/.fzf/bin/fzf" ]
+}
+install_wrapper "fzf" install_fzf exists_fzf
+
 # neovim
 # this installer assumes all the lazy installations into `agnvim` appname to avoid
 # clashes with other installs
@@ -16,18 +73,4 @@ exists_neovim() {
   [ -x "$(command -v nvim)" ] && nvim --version | grep -q "0.10.2"
 }
 install_wrapper "neovim" install_neovim exists_neovim
-
-# rg
-install_ripgrep() {
-    [ -d "$HOME/.local/bin/ripgrep-install" ] && rm -rf "$HOME/.local/bin/ripgrep-install"
-    mkdir -p "$HOME/.local/bin/ripgrep-install"
-    cd $HOME/.local/bin/ripgrep-install \
-    && curl -LO 'https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz' \
-    && tar xzvf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz \
-    && ln -s --force "$(realpath ./ripgrep-13.0.0-x86_64-unknown-linux-musl/rg)" "$HOME/.local/bin/rg"
-}
-exists_ripgrep() {
-    [ -x "$(command -v rg)" ]
-}
-install_wrapper "ripgrep" install_ripgrep exists_ripgrep
 
